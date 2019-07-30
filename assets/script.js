e0b44e3190346da643265a23783c7771bd290fea
Ali Kaafarani <ali@kvikshaug.no>
2019-07-30 12:28:16 +0200

    External script asset

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const images = ["hello1.jpeg", "hello2.jpeg", "hello3.jpeg", "hello4.jpeg", "hello5.jpeg", "hello6.jpeg"];
+  const todaysImage = images[(new Date().getDate() % images.length)];
+  const image = document.getElementById("profile");
+  image.setAttribute("src", "assets/" + todaysImage);
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-64774283-2', 'auto');
+ga('send', 'pageview');
