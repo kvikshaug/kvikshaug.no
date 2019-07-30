924f74fb3b9126f66ec21b17879782f96e4d8507
Ali Kaafarani <ali@kvikshaug.no>
2019-07-30 15:07:37 +0200

    Show a static image

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const images = ["hello1.jpeg", "hello2.jpeg", "hello3.jpeg", "hello4.jpeg", "hello5.jpeg", "hello6.jpeg"];
-  const todaysImage = images[(new Date().getDate() % images.length)];
-  const image = document.getElementById("profile");
-  image.setAttribute("src", "assets/" + todaysImage);
-});
