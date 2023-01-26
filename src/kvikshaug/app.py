0761722dabc391825761c2f9b9e47f942f9de9dc
Ali Kaafarani <ali@kvikshaug.no>
2023-01-26 21:06:51 +0100

    Add 404 page

--- src/kvikshaug/app.py
+++ src/kvikshaug/app.py
@@ -13,6 +13,11 @@ if app.config["DEBUG"]:
     app.jinja_env.undefined = StrictUndefined
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
 @app.route("/")
 def home():
     return render_template("layout.html")
